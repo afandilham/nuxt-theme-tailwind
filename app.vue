75f45b36bd15a2dc7e4c95d53adc6e87dfad6f48
afandilham <afandilham@gmail.com>
2023-03-08 17:32:19 +0700

    first commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="h-screen bg-gray-300 dark:bg-gray-800 transition-all grid place-items-center">
+    <div class="flex gap-4 items-center justify-center">
+      <button class="font-semibold text-sm border-none outline-none px-6 py-2 rounded-sm bg-white" @click="changeTheme('light')">Light</button>
+      <button class="font-semibold text-sm border-none outline-none px-6 py-2 rounded-sm bg-gray-600 text-white" @click="changeTheme('dark')">Dark</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const colorMode = useColorMode();
+
+const changeTheme = (color) => {
+  colorMode.preference = color;
+  console.log(colorMode.preference);
+};
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ["@nuxtjs/color-mode", "@nuxtjs/tailwindcss"],
+  colorMode: {
+    classSuffix: ''
+  },
+});
